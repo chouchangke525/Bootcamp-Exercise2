1c9fe29eaa8e74c803e341dc9e07edbf68129ee2
T. Andrew Binkowski <abinkowski@uchicago.edu>
2012-11-10 17:35:07 -0600

    Initial Commit

---
+++ TrimTheTree/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrimTheTree
+//
+//  Created by T. Andrew Binkowski on 11/10/12.
+//  Copyright (c) 2012 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrimTheTree/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TrimTheTree
+//
+//  Created by T. Andrew Binkowski on 11/10/12.
+//  Copyright (c) 2012 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
